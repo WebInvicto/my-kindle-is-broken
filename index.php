342b59fb87321139efd5ee68ef1d53b4484ab87c
KevInvicto <kevinvicto139@gmail.com>
2020-10-30 16:09:50 +0100

    Etape 1 ok: détails dans les commentaires du code

--- index.php
+++ index.php
@@ -18,7 +18,32 @@
                     </div>
                 </div>
                 <div class="screen_content">
-                  <!-- Mettre le contenu à afficher dans cette div -->
+                  <!-- Ajout du h1 -->
+                  <h1 class="title">Ma liste de livres</h1>
+
+                  <!-- Ajout d'une div qui va me permettre de gérer la mise en forme des images-->
+                  <div class="covers">
+                    <?php 
+                    
+                    // Je dois récupérer le fichier dans lequel se trouve la variable dont j'ai besoin.
+                    require 'books.php'; 
+                    
+                    // Ensuite je dois creer une image cliquable (anticipation etape 2) pour chaque élément du tableau $books.
+                    // Je creer donc une boucle foreach.
+                    foreach($books as $index => $book) : 
+                    
+                    ?>
+                        <!-- A chaque itération, un lien contenant l'image de l'élément courant sera créé --> 
+                        <a href="#"><img src="images/<?= $index ?>.jpg" alt="Couverture du livre <?= $book["titre"] ?>"></a>
+                    
+                    <!-- Je n'oublie pas de fermé la boucle -->
+                    <?php endforeach; ?>
+
+                    <!-- J'ajoute le bouton pour changer la vue (non fonctionnel pour l'instant) -->
+                    <button class="btn">Changer de vue</button>
+                  </div>
+                    
+
                 </div>
             </div>
             <h2>Kindle</h2>
